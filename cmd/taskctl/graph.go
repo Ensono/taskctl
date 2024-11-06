f3b562271dad6a42cb8a1fb293b80283289c56d5
dnitsch <nitschneiderd@gmail.com>
2024-11-06 14:12:44 +0000

    fix: reduce copmlexity in graph drawing

--- cmd/taskctl/graph.go
+++ cmd/taskctl/graph.go
@@ -81,77 +81,109 @@ func anchorName(v string) string {
 // Same nodes can be called multiple times, this relationship of nested graphs (pipelines)
 // is denoted via different relationship arrows/colours.
 func draw(g *dot.Graph, p *scheduler.ExecutionGraph, topLevelStages []string, parent string) {
-	// for k, v := range p.Nodes() {
 	for _, v := range p.BFSNodesFlattened(scheduler.RootNodeName) {
 		if v.Pipeline != nil {
 			// check if subgraph has been added and all it's children
-			psub, _ := g.Root().FindSubgraph(v.Pipeline.Name())
-			vsub, _ := g.Root().FindSubgraph(v.Name)
-			// if sub, found := g.Root().FindSubgraph(v.Pipeline.Name()); found {
-			if psub != nil || vsub != nil {
-				sub := psub
-				if sub == nil {
-					sub = vsub
-				}
-				anchorNode := getNode(sub, anchorName(v.Pipeline.Name()))
-				parentNode := getNode(g, v.Name)
-				if parentNode == nil {
-					// create the pipeline pointer node in the subgraph
-					// if it does not exist
-					pn := g.Node(v.Name)
-					parentNode = &pn
-				}
-				if anchorNode != nil && parentNode != nil {
-					g.Root().Edge(*parentNode, *anchorNode).Attr("color", "brown")
-				}
+			if sub := getSubGraph(g, v); sub != nil {
+				linkSubGraph(g, sub, v)
 			} else {
-				// hoist the subgraph to the top
-				cluster := g.Root().Subgraph(v.Pipeline.Name(), dot.ClusterOption{})
-				anchorNode := cluster.Node(anchorName(v.Pipeline.Name())).Attr("shape", "point").Attr("style", "invis")
-				// loop through subgraph - by adding edges to it
-				draw(cluster, v.Pipeline, topLevelStages, v.Pipeline.Name())
-				// add edge fom parent graph to subgraph cluster
-				var edge dot.Edge
-				if pipelineNode := getNode(g, v.Name); pipelineNode != nil {
-					edge = g.Edge(*pipelineNode, anchorNode)
-				} else {
-					edge = g.Edge(g.Node(v.Name), anchorNode)
-				}
-				edge.Attr("color", "brown")
+				createSubGraph(g, v, topLevelStages)
 			}
 		}
 
-		if v.Task != nil && len(v.DependsOn) == 0 && len(p.Children(v.Name)) == 0 {
-			// since we are flattening all subgprahs we need to create the edge from the root
-			// a relationship will be drawn from the top level  stage/job to subgraph
-			// so we remove the edge relationship line
-			g.Root().Edge(g.Root().Node(parent), g.Node(v.Name)).Attrs("style", "invis")
-			// check if task already exist in the graph
-			taskNode := getNode(g.Root(), v.Name)
-			// check if parent is another pipeline
-			// locate the anchor of the pipeline
-			parentSubgraph := getNode(g.Root(), anchorName(parent))
-			if parentSubgraph != nil && taskNode != nil {
-				// if parentGRaph anchor and task already exist
-				// then we point to it from the subgrpah anchor
-				g.Edge(*parentSubgraph, *taskNode).Attr("color", "green")
-			}
-		}
+		annotateLeaf(g, p, v, parent)
 
 		for _, child := range p.Children(v.Name) {
-			var edge dot.Edge
-			if parent := getNode(g, v.Name); parent != nil {
-				edge = g.Edge(*parent, g.Node(child.Name))
-			} else {
-				edge = g.Edge(g.Node(v.Name), g.Node(child.Name))
-			}
-			if slices.Contains(topLevelStages, v.Name) {
-				edge.Attr("color", "blue")
-			} else {
-				edge.Attr("color", "green")
-			}
+			addEdges(g, v, child, topLevelStages)
 		}
 	}
+}
+
+//
+// Helpers for draw function
+//
+
+// annotateLeaf checks if a stage is a Task and has no parents and no children
+// this would be the
+func annotateLeaf(g *dot.Graph, p *scheduler.ExecutionGraph, v *scheduler.Stage, parent string) {
+	if v.Task != nil && len(v.DependsOn) == 0 && len(p.Children(v.Name)) == 0 {
+		// since we are flattening all subgprahs we need to create the edge from the root
+		// a relationship will be drawn from the top level  stage/job to subgraph
+		// so we remove the edge relationship line
+		g.Root().Edge(g.Root().Node(parent), g.Node(v.Name)).Attrs("style", "invis")
+		// check if task already exist in the graph
+		taskNode := getNode(g.Root(), v.Name)
+		// check if parent is another pipeline
+		// locate the anchor of the pipeline
+		parentSubgraph := getNode(g.Root(), anchorName(parent))
+		if parentSubgraph != nil && taskNode != nil {
+			// if parentGRaph anchor and task already exist
+			// then we point to it from the subgrpah anchor
+			g.Edge(*parentSubgraph, *taskNode).Attr("color", "green")
+		}
+	}
+}
+
+// getSubGraph
+func getSubGraph(g *dot.Graph, v *scheduler.Stage) *dot.Graph {
+	var sub *dot.Graph
+	var found bool
+
+	// check if subgraph has been added and all it's children
+	if sub, found = g.Root().FindSubgraph(v.Pipeline.Name()); !found {
+		if sub, found = g.Root().FindSubgraph(v.Name); !found {
+			return nil
+		}
+	}
+	return sub
+}
+
+// addEdges adds the task/pipeline nodes to the parent
+func addEdges(g *dot.Graph, v *scheduler.Stage, child *scheduler.Stage, topLevelStages []string) {
+	var edge dot.Edge
+	if parent := getNode(g, v.Name); parent != nil {
+		edge = g.Edge(*parent, g.Node(child.Name))
+	} else {
+		edge = g.Edge(g.Node(v.Name), g.Node(child.Name))
+	}
+	if slices.Contains(topLevelStages, v.Name) {
+		edge.Attr("color", "blue")
+	} else {
+		edge.Attr("color", "green")
+	}
+}
+
+// linkSubGraph connects an anchor to an already created subgraph.
+// Creating or using an existing parentNode
+func linkSubGraph(g *dot.Graph, sub *dot.Graph, v *scheduler.Stage) {
+	anchorNode := getNode(sub, anchorName(v.Pipeline.Name()))
+	parentNode := getNode(g, v.Name)
+	if parentNode == nil {
+		// create the pipeline pointer node in the subgraph
+		// if it does not exist
+		pn := g.Node(v.Name)
+		parentNode = &pn
+	}
+	if anchorNode != nil && parentNode != nil {
+		g.Root().Edge(*parentNode, *anchorNode).Attr("color", "brown")
+	}
+}
+
+// createSubGraph adds a new subgraph to the root graph
+func createSubGraph(g *dot.Graph, v *scheduler.Stage, topLevelStages []string) {
+	// hoist the subgraph to the top
+	cluster := g.Root().Subgraph(v.Pipeline.Name(), dot.ClusterOption{})
+	anchorNode := cluster.Node(anchorName(v.Pipeline.Name())).Attr("shape", "point").Attr("style", "invis")
+	// loop through subgraph - by adding edges to it
+	draw(cluster, v.Pipeline, topLevelStages, v.Pipeline.Name())
+	// add edge fom parent graph to subgraph cluster
+	var edge dot.Edge
+	if pipelineNode := getNode(g, v.Name); pipelineNode != nil {
+		edge = g.Edge(*pipelineNode, anchorNode)
+	} else {
+		edge = g.Edge(g.Node(v.Name), anchorNode)
+	}
+	edge.Attr("color", "brown")
 }
 
 // getNode helper looks a node by Id and falling back on to label if not found
