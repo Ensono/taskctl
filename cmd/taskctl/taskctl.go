b504190ae044a50e88df8683e4381459e04d7601
dnitsch <nitschneiderd@gmail.com>
2025-01-15 16:07:07 +0000

    fix: add os Signals listener (#24)
    * fix: add os Signals listener
    
    * fix: fmt on utils
    
    * fix: remove error return from go func
    
    * fix: add tests for cancel
    
    * fix: add additional signals

--- cmd/taskctl/taskctl.go
+++ cmd/taskctl/taskctl.go
@@ -193,10 +193,14 @@ func (tc *TaskCtlCmd) buildTaskRunner(args []string, conf *config.Config) (*runn
 	// These are stdin args passed over `-- arg1 arg2`
 	vars.Set("ArgsList", argsStringer.argsList)
 	vars.Set("Args", strings.Join(argsStringer.argsList, " "))
-	tr, err := runner.NewTaskRunner(runner.WithContexts(conf.Contexts), runner.WithVariables(vars), func(runner *runner.TaskRunner) {
-		runner.Stdout = tc.ChannelOut
-		runner.Stderr = tc.ChannelErr
-	})
+	tr, err := runner.NewTaskRunner(runner.WithContexts(conf.Contexts),
+		runner.WithVariables(vars),
+		func(runner *runner.TaskRunner) {
+			runner.Stdout = tc.ChannelOut
+			runner.Stderr = tc.ChannelErr
+			runner.Stdin = tc.Cmd.InOrStdin()
+		},
+		runner.WithGracefulCtx(tc.Cmd.Context()))
 
 	if err != nil {
 		return nil, nil, err
